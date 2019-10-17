5e1a236a4eec5aae8377ad741f7f2c5ac81c6bdb
Federico Cattini <b994@live.com>
2019-10-17 15:28:22 +0200

    - todo

--- Canvas2FallingRectangle/Scripts/superClasses.js
+++ Canvas2FallingRectangle/Scripts/superClasses.js
@@ -1,3 +1,9 @@
+
+//TO DO: create vector2D class with vector functions and fix the code
+//TO DO: complete isColliding function for squares
+//Maybe some machine learning
+
+//God class
 class BoxCollider2D{
   constructor(x, y, width, height){
     this.x = x;
@@ -11,6 +17,7 @@ class BoxCollider2D{
   }
 }
 
+//Class for physical objects
 class PhysicalObject2D extends BoxCollider2D{
   constructor(x, y, width, height){
     super(x, y, width, height);
